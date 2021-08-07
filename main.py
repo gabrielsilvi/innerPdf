3b7184e239ca90e9532d3effb4da2acf81818627
gabrielsilvi <gabrielsv3000@hotmail.com>
2021-08-07 13:59:01 -0300

    v1.0 em testes

--- main.py
+++ main.py
@@ -5,16 +5,30 @@ import PyPDF2
 print("Digite quantos arquivos vai juntar...")
 print("Ou digite 'ex' para cancelar")
 nArquivos=int(input())
-print(type(nArquivos))
+print("Escolhendo arquivos...")
+merge = PyPDF2.PdfFileMerger()
 if (type(nArquivos)==int):
-    print("Escolhendo arquivos...")
+    merge = PyPDF2.PdfFileMerger()
+    contador = 1
+    while(contador<=nArquivos):
+        print("Escolhendo arquivo "+str(contador))
+        arq=open(input(),'rb')
+        merge.append(PyPDF2.PdfFileReader(arq))
+        if(contador<nArquivos):
+            merge.write(r'C:\Users\gabri\Desktop\novoArquivo.pdf')
+        contador+=1
+
+    print("Concluido!")
 
 
-elif(nArquivos=="ex"):
-    print("Saindo...")
-elif(type(nArquivos) != int):
-    print("Entrada Invalida!...")
+# for i in range(1,nArquivos+1):
+#     local='arq'+str(i)
 
+# elif(nArquivos=="ex"):
+#     print("Saindo...")
+# elif(type(nArquivos) != int):
+#     print("Entrada Invalida!...")
+#
 
 # arq1=r'C:\Users\gabri\Desktop\teste.pdf'
 # arq2=r'C:\Users\gabri\Desktop\teste2.pdf'
